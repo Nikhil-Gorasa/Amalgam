4a3ed5735d7c3e3ac7df8514b2aaae75e345585e
Nikhil Gorasa <nikhil.gorasa1@gmail.com>
2025-01-15 15:29:13 +0530

    fix: update CORS configuration for production deployment

--- SpringBoot/portfoliotracker/src/main/java/com/amalgam/portfoliotracker/config/WebConfig.java
+++ SpringBoot/portfoliotracker/src/main/java/com/amalgam/portfoliotracker/config/WebConfig.java
@@ -9,12 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        // .allowedOrigins("http://localhost:5173")
-            .allowedOrigins("*")
+            .allowedOrigins("https://amalgam-finance.netlify.app")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
-            
-            // .allowCredentials(true);
-            .allowCredentials(false);
+            .allowCredentials(true);
     }
 } 
